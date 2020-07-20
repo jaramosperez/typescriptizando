c125b0fdedb0e1d2ca1a2e6492340e927422f0c4
Javier Ramos <jaramosperez85@gmail.com>
2020-07-20 00:35:03 -0400

    Tipos Primera Parte

---
+++ Basic Types/Types.js
@@ -0,0 +1,21 @@
+/* Comenzando con los Tipos Boolean
+Es de los tipos de datos mas basicos es simple
+es true/false (Verdadero/Falso)*/
+var isDone = false;
+console.log(isDone);
+/* Comenzando con los Tipos Number
+Considera los flotantes como number y los bingInt como bigint*/
+var decimal = 6;
+var hex = 0xf00d;
+var binary = 10;
+var octal = 484;
+//let big: bigint = 100n;
+/*Los Famosos Strings, fundamentales en todos
+los lenguaje. Se usa la palabra string para definirlos */
+var texto = "Aprendiendo como siempre!!";
+//color = "Red";
+/*Otra forma de trabajar con los string */
+var fullName = "Bob Bobbington";
+var age = 37;
+var sentence = "Hola, mi nombre es " + fullName + ".\nel mes que viene cumplire " + age;
+console.log(sentence);
---
+++ Basic Types/Types.tsx
@@ -0,0 +1,52 @@
+/* Comenzando con los Tipos Boolean 
+Es de los tipos de datos mas basicos es simple
+es true/false (Verdadero/Falso)*/
+let isDone: boolean = false
+console.log(isDone)
+
+/* Comenzando con los Tipos Number 
+Considera los flotantes como number y los bingInt como bigint*/
+let decimal: number = 6;
+let hex: number = 0xf00d;
+let binary: number = 0b1010
+let octal: number = 0o744;
+//let big: bigint = 100n;
+
+/*Los Famosos Strings, fundamentales en todos 
+los lenguaje. Se usa la palabra string para definirlos */
+
+let texto: string = "Aprendiendo como siempre!!";
+//color = "Red";
+
+/*Otra forma de trabajar con los string */
+let fullName: string = `Bob Bobbington`;
+let age: number = 37;
+let sentence: string = `Hola, mi nombre es ${fullName}.
+el mes que viene cumplire ${age}`
+
+console.log(sentence)
+
+/* let sentence: string =
+  "Hello, my name is " +
+  fullName +
+  ".\n\n" +
+  "I'll be " +
+  (age + 1) +
+  " years old next month."; */
+
+  /*Los Array o arreglos, listados, como quieras llamarlos
+  si vienes de otros lenguajes.
+  Tenemos los arrays por tipos */
+
+  let listado: number[] = [1, 2, 3];
+  let listadoNumerico: Array<number> = [1, 2, 3];
+
+  /*Tuplas 
+  En estos tipos de datos debemos pasar los elementos segun su tipo y en el orden declarado al crear la tupla*/
+
+// Declaración de una tupla
+let soyUnaTupla: [string, number];
+// Inicializar
+soyUnaTupla = ["hello", 10]; // OK
+// No inicializa por error en el orden de los elementos pasados.
+// soyUnaTupla = [10, "hello"]; // Error
